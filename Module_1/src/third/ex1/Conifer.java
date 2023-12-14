6908d316185a7b995c8145d1ca6da54d6b073323
Godrik <krisapios@gmail.com>
2023-12-14 17:49:26 +0500

    HM 3

--- Module_1/src/third/ex1/Conifer.java
+++ Module_1/src/third/ex1/Conifer.java
@@ -3,12 +3,9 @@ package third.ex1;
 public abstract class Conifer extends Tree {
     protected String needles;
 
+
     public Conifer(int age, String needles) {
         super(age);
         this.needles = needles;
-    }
-
-    public String getNeedles() {
-        return needles;
     }
 }
---
+++ Module_1/src/third/ex3/Pyramid2.java
@@ -0,0 +1,9 @@
+package third.ex3;
+
+public class Pyramid2 extends Pyramid3{
+    @Override
+    public void print() {
+        System.out.println("ММ");
+        super.print();
+    }
+}
